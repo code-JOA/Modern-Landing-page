a4dbc8927b23deab69c2f15146dd6a0612e94e07
Joshua Owusu <joshbillion1@gmail.com>
2023-07-18 12:55:43 +0000

    Commit auto_update

--- Landing-Page/script.js
+++ Landing-Page/script.js
@@ -3,25 +3,28 @@ $("#anim").textillate({ in: { effect: "fadeInOut", delay: 200 } });
 const t1 = new gsap.timeline();
 
 t1.from(".lrow a", {
-  stagger: .2,
+  stagger: 0.2,
   opacity: 0,
   x: 10,
 });
 
-.from("#playbtn .anm", {
-  stagger: .2,
-  opacity: 0,
-  x: 10,
-}, '-=1');
+t1.from(
+  "#playbtn .anm",
+  {
+    stagger: 0.2,
+    opacity: 0,
+    x: 10,
+  },
+  "-=1"
+);
 
-.from("#hero img", {
-  opacity: 0,
-  y: 20,
-  duration: 2,
-  ease: "Expo.easeInOut"
-}, '-=1.5');
-
-
-
-
-
+t1.from(
+  "#hero img",
+  {
+    opacity: 0,
+    y: 20,
+    duration: 2,
+    ease: "Expo.easeInOut",
+  },
+  "-=1.5"
+);
